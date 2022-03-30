173473f13f1b5578c09870067b8c64eadbb062b5
Leonid <ewro01@yandex.ru>
2022-03-30 14:19:37 +0300

    3 30 2022

--- app/src/main/java/com/example/fancontrol/MainActivity.java
+++ app/src/main/java/com/example/fancontrol/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.fancontrol;
 
 import android.annotation.SuppressLint;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
+import android.bluetooth.*;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.view.LayoutInflater;
@@ -21,15 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     Button bconnect;
     TextView logview;
-    //ListView listView;
+    ListView listDevice;
     BluetoothDevice controller;
     BluetoothManager btManager;
     boolean stateconnection = false;
     BluetoothAdapter btAdapter;
     String addressBTController = "00:00:00:00";
 
-    List<String> paredDevicelist;
-    ArrayAdapter<String> arrayListAdapter;
+    Set<BluetoothDevice> paredDev;
+    ArrayList pairedDeviceArrayList;
+    ArrayAdapter< String> pairedDeviceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bconnect = (Button) findViewById(R.id.buttonconnect);
         logview = (TextView) findViewById(R.id.loggingview);
+        listDevice = (ListView) findViewById(R.id.listdevice);
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
             Toast.makeText(this, "You device BLUETOOTH not support", Toast.LENGTH_LONG).show();
@@ -44,14 +44,17 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        System.out.println(btAdapter);
+        btManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        btAdapter = btManager.getAdapter();
+
+        //System.out.println(btAdapter);
         if (btAdapter == null) {
             Toast.makeText(this, "Bluetooth is not supported on this hardware platform", Toast.LENGTH_LONG).show();
             finish();
             return;
         }
-        String stInfo = btAdapter.getName() + " = " + btAdapter.getAddress();
+        String stInfo = btAdapter.getName() + " = " + btAdapter.getAddress() + "State bt module = " + btAdapter.getState();
         logview.setText(String.format("Это устройство: %s", stInfo));
     }
 
@@ -63,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableIntent, 1);
         }
+
     }
 
     @SuppressLint("SetTextI18n")
@@ -71,5 +75,56 @@ public class MainActivity extends AppCompatActivity {
         logview.setText( s + "\n connect");
        // BluetoothDevice device2 = btAdapter.getRemoteDevice(addressBTController);
         //start thread connecting
+        //if(pairedDeviceArrayList == null)
+            fillList();
+    }
+
+    public void fillList(){
+        paredDev = btAdapter.getBondedDevices();
+        if (paredDev.size() > 0) { // Если есть сопряжённые устройства
+            pairedDeviceArrayList = new ArrayList<>();
+            for (BluetoothDevice device : paredDev) { // Добавляем сопряжённые устройства - Имя + MAC-адресс
+                pairedDeviceArrayList.add(device.getName() + "\n" + device.getAddress());
+            }
+            pairedDeviceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pairedDeviceArrayList);
+            listDevice.setAdapter(pairedDeviceAdapter);
+            listDevice.setOnItemClickListener(new AdapterView.OnItemClickListener() { // Клик по нужному устройству
+                @Override
+                public void onItemClick(AdapterView< ?> parent, View view, int position, long id) { //тут пробел после скобки !!!!
+                    //listDevice.setVisibility(View.GONE); // После клика скрываем список
+                    String  itemValue = (String) listDevice.getItemAtPosition(position);
+                    String MAC = itemValue.substring(itemValue.length() - 17); // Вычленяем MAC-адрес
+                    BluetoothDevice device2 = btAdapter.getRemoteDevice(MAC);
+                    String tmp = (String) logview.getText();
+                    logview.setText(tmp + MAC);
+                    //myThreadConnectBTdevice = new ThreadConnectBTdevice(device2);
+                    //myThreadConnectBTdevice.start();  // Запускаем поток для подключения Bluetooth
+                }
+            });
+        }
+        /*
+        paredDev = btAdapter.getBondedDevices();
+        ArrayList<String> list = new ArrayList<String>();
+        for(BluetoothDevice bt : paredDev) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        listDevice.setAdapter(adapter);
+        */
+    }
+
+    class BTFanConnection extends Thread{
+        BluetoothSocket btSocket = null;
+        BluetoothDevice btDevice;
+
+        public BTFanConnection(BluetoothDevice myDevice) {
+            btDevice = myDevice;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+
+        }
     }
 }
